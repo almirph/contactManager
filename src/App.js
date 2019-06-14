0cd067450d4bec697b409a1493b4ca18ca8c4125
almir.arruda <almir.arruda@ntendencia.com.br>
2019-06-14 09:25:42 -0300

    Comit inicial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Contacts from "./components/Contacts/Contacts";
+import Header from "./components/Layout/Header";
+import AddContact from "./components/Contacts/AddContact";
+import { Provider } from "./Context";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./components/Pages/About";
+import NotFound from "./components/Pages/NotFound";
+import EditContact from "./components/Contacts/EditContact";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <Router>
+          <div>
+            <Header />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/contact/edit/:id" component={EditContact} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
